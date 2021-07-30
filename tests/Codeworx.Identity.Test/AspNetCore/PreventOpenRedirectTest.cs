18a62ced4541522e58b0748c17144518577f1142
Raphael Schwarz <raphael@codeworx.at>
2021-07-30 15:50:30 +0200

    OpenRedirectTest WIP

---
+++ tests/Codeworx.Identity.Test/AspNetCore/PreventOpenRedirectTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework;
+
+namespace Codeworx.Identity.Test.AspNetCore
+{
+    public class PreventOpenRedirectTest : IntegrationTestBase
+    {
+        public async Task WindowsLogin_ExternalReturnUrl_Expects_BadRequest()
+        {
+            var uriBuilder = new UriBuilder(TestClient.BaseAddress.ToString());
+            uriBuilder.AppendPath("winlogin/78275C4F-EF6E-46D3-88B6-80CAC6B6D1F8");
+            uriBuilder.AppendQueryParameter("returnUrl", "https://example.org/bla");
+
+            var tokenResponse = await this.TestClient.GetAsync(uriBuilder.ToString());
+
+            Assert.AreEqual(StatusCodes.Status400BadRequest, tokenResponse.StatusCode);
+        }
+
+        public async Task WindowsLogin_RelativeInternalReturnUrl_Expects_Redirect()
+        {
+            var uriBuilder = new UriBuilder(TestClient.BaseAddress.ToString());
+            uriBuilder.AppendPath("winlogin/78275C4F-EF6E-46D3-88B6-80CAC6B6D1F8");
+            uriBuilder.AppendQueryParameter("returnUrl", "/account/login");
+
+            var tokenResponse = await this.TestClient.GetAsync(uriBuilder.ToString());
+
+            Assert.AreEqual(StatusCodes.Status304NotModified, tokenResponse.StatusCode);
+        }
+    }
+}
